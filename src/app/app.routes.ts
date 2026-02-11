2b5973757581b91548e41bf2a9f342c60799d22a
saritagore21@gmail.com <rahulgore34@gmail.com>
2026-02-11 17:24:43 +0530

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  }
+];
