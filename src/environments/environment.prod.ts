d5050aed81b79de7a70cac456e60790661f76e83
saritagore21@gmail.com <rahulgore34@gmail.com>
2026-02-18 18:05:07 +0530

    staging env added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  envName: 'production',
+  apiUrl: 'https://prod-api.azurewebsites.net'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  envName: 'local',
+  apiUrl: 'http://localhost:3000'
+};
